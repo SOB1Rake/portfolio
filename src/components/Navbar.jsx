0a2cada757996470112d96564c3a606162c172c7
sobirjon <sobirjonotajonov11@gmail.com>
2026-03-20 20:35:07 +0500

    navbar is full end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import { Menu } from 'lucide-react';
 
 export default function Navbar() {
   const [lang, setLang] = useState("uz");
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div>
       <nav className='bg-[#0F172A] border border-[#1E293B] py-[20px]'>
@@ -34,17 +35,45 @@ export default function Navbar() {
           <div className='text-[#FFFFFF] bg-[#1E293B80] flex gap-[8px] py-[8px] px-[12px] rounded-[12px] '>
             <div className="flex gap-[4px]">
               <Languages />
-            <select
-              value={lang}
-              onChange={(e) => setLang(e.target.value)}
-              className='appearance-none bg-[#1E293B80] border-none'
-            >
-              <option value="uz">UZ</option>
-              <option value="ru">RU</option>
-              <option value="en">EN</option>
-            </select>
+              <select
+                value={lang}
+                onChange={(e) => setLang(e.target.value)}
+                className='appearance-none bg-[#1E293B80] border-none'
+              >
+                <option value="uz">UZ</option>
+                <option value="ru">RU</option>
+                <option value="en">EN</option>
+              </select>
             </div>
-            <Menu className="md:hidden"/>
+            <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+              <Menu />
+            </button>
+            {/* {menuOpen && (
+              <div className="md:hidden ">
+                <a href="#home">{translations[lang].projects}</a>
+                <a href="#about">{translations[lang].services}</a>
+                <a href="#contact">{translations[lang].contact}</a>
+              </div>
+            )} */}
+            {menuOpen && (
+  <div className="md:hidden absolute top-[70px] right-[-112px] w-[50%] px-4">
+    <div className="bg-[#0F172A] rounded-xl shadow-lg p-4 flex flex-col gap-3 w-[50%]">
+      
+      <a href="#!" className="hover:text-blue-500 transition">
+        {translations[lang].projects}
+      </a>
+
+      <a href="#!" className="hover:text-blue-500 transition">
+        {translations[lang].services}
+      </a>
+
+      <a href="#!" className="hover:text-blue-500 transition">
+        {translations[lang].contact}
+      </a>
+
+    </div>
+  </div>
+)}
           </div>
         </div>
       </nav>
